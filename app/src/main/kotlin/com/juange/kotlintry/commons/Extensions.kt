6433fd388bd27f34fbd931d9dab8e811d71e6635
Juan Jesús Fernández Madrid <juangefm@gmail.com>
2016-11-21 19:00:28 +0100

    add extension class

---
+++ app/src/main/kotlin/com/juange/kotlintry/commons/Extensions.kt
@@ -0,0 +1,10 @@
+package com.juange.kotlintry.commons
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, false)
+}
+
